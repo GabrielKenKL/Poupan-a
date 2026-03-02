768e0fd343dd9a0c3e062c98ccd70a0ac8267807
Gabriel Ken Kudo de Lima <gabkenkudo@gmail.com>
2026-03-02 09:32:43 -0300

    Primeira versão dos códigos.

---
+++ Poupança.py
@@ -0,0 +1,7 @@
+print("Cálculo da renda de um valor depositado na poupança após 1 mês de aplicação e rendendo 1,3% ao mês.")
+print("Digite o valor aplicado:")
+
+Valor = int(input())
+renda = Valor*(13*10**-3)
+
+print("A renda após 1 mês de aplicação equivale a:" , f"{renda:.2f}")
